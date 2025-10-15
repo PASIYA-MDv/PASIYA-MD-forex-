d806fb0d3844570a43274b841c5524201cda30cc
PASIYA-MDv <pasidusampath120@gmail.com>
2025-10-15 07:50:59 +0530

    Create logger.py

---
+++ # utils/logger.py
@@ -0,0 +1,11 @@
+# utils/logger.py
+import logging
+def get_logger(name='PASIYA-MD'):
+    logger = logging.getLogger(name)
+    if not logger.handlers:
+        handler = logging.StreamHandler()
+        formatter = logging.Formatter('[%(asctime)s] %(levelname)s - %(message)s')
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+        logger.setLevel(logging.INFO)
+    return logger
